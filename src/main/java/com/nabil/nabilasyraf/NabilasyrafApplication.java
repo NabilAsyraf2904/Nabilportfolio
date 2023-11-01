65a347b03571acf889a9c274277b9d7eddadf03e
NabilAsyraf2904 <nabilasyraf215@gmail.com>
2023-11-01 21:17:54 +0800

    new

--- src/main/java/com/nabil/nabilasyraf/NabilasyrafApplication.java
+++ src/main/java/com/nabil/nabilasyraf/NabilasyrafApplication.java
@@ -1,25 +1,16 @@
 package com.nabil.nabilasyraf;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class NabilasyrafApplication {
 
-	@Autowired
-	private EmailSenderService senderService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(NabilasyrafApplication.class, args);
 	}
-	
-	@EventListener(ApplicationReadyEvent.class)
-	public void sendMail() {
-		senderService.sendEmail("nabilasyraf215@gmail.com",
-				"This is Subject", "This is Body of Email");
-	}
+
 
 }
